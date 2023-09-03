e9f169f820aa615af7d19ad879acc30148a4dcd4
黄棋烨 <498618798@qq.com>
2023-09-03 17:11:08 +0800

    小修改

---
+++ client/create_share.py
@@ -0,0 +1,14 @@
+
+
+
+# 创建共享空间函数
+def create_shared_space(username,space_name):
+    message = "create_shared_space|" + username + "|" + space_name
+    return message
+
+# 将用户添加到共享空间函数
+def add_user_to_shared_space(space_name,username):
+    message = "add_user_to_shared_space|" + space_name + "|" + username
+    return message
+
+
