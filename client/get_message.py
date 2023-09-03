7e1ac62889f8c7aa7da217305c123d53d883e328
q2564068073 <83351598+q2564068073@users.noreply.github.com>
2023-09-03 11:13:56 +0800

    小修改

--- client/get_message.py
+++ client/get_message.py
@@ -1,26 +1,46 @@
 #from users import Users
 #user = Users()
+import socket
+import hashlib
+import json
+import re
+import base64
+from Crypto import Random
+from Crypto.PublicKey import RSA
+import time
 class Registration:
   def __init__(self):
     self.users = []
 
   def get_message(self, username, password,email,phone_number):
-    print(username,password,email,phone_number)
-    message = "register|" + username + "|" + password + "|" + email + "|" + phone_number
+    k=check_password(password)
+    if k == 2:
+      return False
+    e=checkemail(email)
+    if e==2:
+      return False
+    p=chec_phone_number(phone_number)
+    if p==2:
+      return False
+    md=hashlib.md5(password.encode('utf-8'))
+    md=md.hexdigest()
+    print(username,md,email,phone_number)
+    message = "register|" + username + "|" + md + "|" + email + "|" + phone_number
     return message
 
 class LoginName:
   def __init__(self):
     return None
   def get_message(self, username, password):
-    print(username, password)
-    message = "login_password|" + username + "|" + password
+    md = hashlib.md5(password.encode('utf-8'))
+    md=  md.hexdigest()
+    print(username, md)
+    message = "login_password|" + username + "|" + md
     return message
 
 class LoginEmail:
   def __init__(self):
     return None
-
   def get_message(self, email, email_code):
     print(email, email_code)
     message = "login_email|" + email + "|" + email_code
@@ -28,6 +48,39 @@ class LoginEmail:
 
 class ForgetPassword:
   def get_message(self, phone_number, phone_code, new_password1, new_password2):
-    print(phone_code, phone_number, new_password1, new_password2)
-    message = "change_password|" + phone_number + "|" + phone_code + "|" + new_password1 + "|" + new_password2
+    md1= hashlib.md5(new_password1.encode('utf-8'))
+    md1=  md1.hexdigest()
+    md2= hashlib.md5(new_password2.encode('utf-8'))
+    md2=  md2.hexdigest()
+    if md1!= md2 :
+      return False
+    print(phone_code, phone_number, md1)
+    message = "change_password|" + phone_number + "|" + phone_code + "|" + md1
     return message
+
+def check_password(password):
+  result = re.compile(r'^(?![a-zA-z]+$)(?!\d+$)(?![!@#$%^&*.?]+$)[a-zA-Z\d!@#$%.?^&*]+$')
+  if re.fullmatch(result, password):
+    k=1
+  else:
+    k=2
+  return k
+
+def checkemail(address):
+  """
+  利用正则表达式检测邮箱格式
+  """
+  regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+  if re.fullmatch(regex, address):
+    t = 1
+  else:
+    t = 2
+  return t
+
+def chec_phone_number(phone_number):
+  result = re.compile(r'^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$')
+  if re.fullmatch(result, phone_number):
+    k=1
+  else:
+    k=2
+  return k
