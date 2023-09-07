945676299cfa123415d89e2c54cbc417cbf8562d
q2564068073 <83351598+q2564068073@users.noreply.github.com>
2023-09-07 14:05:36 +0800

    全部删除

--- server/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-conn = pymysql.connect(
-    host=SERVER_DB['host'],
-    user=SERVER_DB['user'],
-    password=SERVER_DB['password'],
-    database=SERVER_DB['database'],
-    charset=SERVER_DB['charset']
-)
